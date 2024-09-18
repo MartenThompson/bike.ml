18f9a6fabb55f2f01285aeab1ca3d3605412bdeb
marten <marten@Martens-MacBook-Pro-2.local>
2024-09-17 22:47:40 -0700

    gotta have it

---
+++ bike.ml/bike.mlTests/bike_mlTests.swift
@@ -0,0 +1,17 @@
+//
+//  bike_mlTests.swift
+//  bike.mlTests
+//
+//  Created by marten on 9/17/24.
+//
+
+import Testing
+@testable import bike_ml
+
+struct bike_mlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
